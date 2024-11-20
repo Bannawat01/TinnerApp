baa79d6ec20ff3968ed6772a986941327400627f
Bannawat <coach.ra47@gmail.com>
2024-11-20 11:34:06 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { tlsConfig } from "./configs/tls.config"
 import cors from "@elysiajs/cors"
 import { MongoDB } from "./configs/database.config"
 import { jwtConfig } from "./configs/jwt.config"
+import { AccountController } from "./controllers/account.controller"
 
 MongoDB.connect()
 
@@ -12,7 +13,8 @@ const app = new Elysia()
   .use(jwtConfig)
   .use(cors())
   .use(swaggerConfig)
-  .use(example)
+  .use(AccountController)
+  // .use(example)
 
   .listen({
     port: Bun.env.PORT || 8000,

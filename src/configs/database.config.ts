61cc8c79506c2c6a1c28cb3f606456312d4811ba
Bannawat <coach.ra47@gmail.com>
2024-11-20 09:26:07 +0700

    init

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,18 +1,18 @@
 import mongoose from "mongoose"
 
 const username = Bun.env.MONGGO_DB_USER || 'bannawatra'
-const password = Bun.env.MONGGO_DB_PASSWORD || '6VHxRvA3sNo7zEf6'
+const password = Bun.env.MONGO_DB_PASS || 'your_pass'
 const db_name = Bun.env.MONGGODB_NAME || 'Tinner_app'
 
-const uri = `mongodb + srv://${username}:${password}@cluster0.akwct.mongodb.net/?retryWrites=true&w=majority&appName=${db_name}`
+const uri = `mongodb+srv://${username}:${password}@cluster0.akwct.mongodb.net/?retryWrites=true&w=majority&appName=${db_name}`
 
 export const MongoDB = {
     connect: async function () {
         try {
             await mongoose.connect(uri)
-            console.log(' MongoDB is connected')
+            console.log('MongoDB is connected')
         } catch (error) {
-            console.error(' MongoDB is not connected')
+            console.error('MongoDB is not connected')
             console.error(error)
         }
     }

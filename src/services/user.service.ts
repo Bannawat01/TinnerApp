26deccd14fb1d2073c240687aac627bd495d6b4c
Bannawat <coach.ra47@gmail.com>
2024-12-04 08:15:53 +0700

    init

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,6 +2,8 @@ import mongoose, { RootFilterQuery } from "mongoose"
 import { updateProfile, user, userPagination, userPaginator } from "../types/user.type"
 import { IUserDocument } from "../interfaces/user.interface"
 import { QueryHelper } from "../helpers/query.helper"
+import { User } from "../models/user.model"
+import { last } from "lodash"
 
 export const UserService = {
     get: function (pagination: userPagination, user_id: string): Promise<userPaginator> {
@@ -9,8 +11,8 @@ export const UserService = {
             _id: { $nin: new mongoose.Types.ObjectId(user_id) },
             $and: QueryHelper.parseUserQuery(pagination)
         }
+        const query = User.find({ filter }).sort({ last_active: -1 }) //descendent order 🟣/ ascendent order
         throw new Error("Not implemented")
-
     },
 
     getByUserName: function (username: string): Promise<user> {

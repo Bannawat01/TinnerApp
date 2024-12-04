656d813e68c9c7ece17d455f61f5113ad3397bfd
Bannawat <coach.ra47@gmail.com>
2024-12-04 11:20:34 +0700

    init

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import Elysia from "elysia"
 import { AuthMiddleWare, AuthPlayload } from "../middlewares/auth.middleware"
 import { UserDto } from "../types/user.type"
 import { UserService } from "../services/user.service"
+import { set } from "mongoose"
 
 export const UserController = new Elysia({
     prefix: '/api/user',
@@ -25,5 +26,23 @@ export const UserController = new Elysia({
         detail: { summary: 'Get user' },
         query: "pagination",
         response: "users",
+        isSignIn: true,
+    })
+
+    .patch('/', async ({ body, set, Auth }) => {
+        try {
+            const user_id = (Auth.payload as AuthPlayload).id //get user_id from token
+            return await UserService.updateProfile(body, (Auth.payload as AuthPlayload).id)
+        } catch (error) {
+            set.status = 400 //bad request
+            if (error instanceof Error)
+                throw new Error(error.message)
+            set.status = 500 // internal server error
+            throw new Error('Something went wrong, try agian later')
+        }
+    }, {
+        detail: { summary: 'Update profile' },
+        body: "update_profile",
+        // response: "user",
         isSignIn: true,
     })
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -34,11 +34,17 @@ export const UserService = {
         this._get = value
     },
 
-    getByUserName: function (username: string): Promise<user> {
-        throw new Error("Not implemented")
-    },
+    // getByUserName: async function (username: string): Promise<user> {
+    //     const user = await User.findOne({ username }).exec()
+    //     if (user)
+    //         return user.toUser()
+    //     throw new Error(`username: "${username}" is not found !!`)
+    // },
 
-    updateProfile: function (newProfile: updateProfile, user_id: string): Promise<user> {
-        throw new Error("Not implemented")
+    updateProfile: async function (newProfile: updateProfile, user_id: string): Promise<user> {
+        const user = await User.findByIdAndUpdate(user_id, { $set: newProfile }, { new: true, runValidators: true })
+        if (user)
+            return user.toUser()
+        throw new Error('Something went wrong,try agian later !!')
     }
 }
